2a2a7862b899bb26649fc10004d898dfebb8162c
mr.sergito <sergioleear@gmail.com>
2024-12-27 20:48:44 +0100

    feat: Global error handling approaches

---
+++ CostCraft.Api/Controllers/ErrorsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CostCraft.Api.Controllers;
+
+public class ErrorsController : ControllerBase
+{
+    [Route("/error")]
+    public IActionResult Error()
+    {
+        Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+        return Problem();
+    }
+}
--- CostCraft.Api/Program.cs
+++ CostCraft.Api/Program.cs
@@ -1,6 +1,7 @@
 using CostCraft.Api.Infrastructure.Data;
 using CostCraft.Application;
 using CostCraft.Infrastructure;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +13,9 @@ builder.Services
     .AddDbContext<CostCraftDbContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("CostCraftDb"))
     );
+//builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
 builder.Services.AddControllers();
+//builder.Services.AddSingleton<ProblemDetailsFactory, CostCraftProblemDetailsFactory>();
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
@@ -20,6 +23,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+// app.UseMiddleware<ErrorHandlingMiddleware>();
+app.UseExceptionHandler("/error");
+app.Map("/error", (HttpContext httpContext) =>
+{
+    Exception? exception = httpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+    return Results.Problem();
+});
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
