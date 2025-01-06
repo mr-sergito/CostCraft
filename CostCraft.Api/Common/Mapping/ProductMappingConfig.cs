54b43ac3c51d6fa791cf2fd7df961124de5d30ac
mr.sergito <sergioleear@gmail.com>
2025-01-06 21:24:29 +0100

    feat: Added missing mappings of Product entities

--- CostCraft.Api/Common/Mapping/ProductMappingConfig.cs
+++ CostCraft.Api/Common/Mapping/ProductMappingConfig.cs
@@ -1,6 +1,7 @@
 ﻿using CostCraft.Application.Products.Commands.CreateProduct;
 using CostCraft.Contracts.Products;
 using CostCraft.Domain.ProductAggregate;
+using CostCraft.Domain.ProductAggregate.Entities;
 using Mapster;
 
 namespace CostCraft.Api.Common.Mapping;
@@ -16,5 +17,11 @@ public class ProductMappingConfig : IRegister
         config.NewConfig<Product, ProductResponse>()
             .Map(dest => dest.Id, src => src.Id.Value)
             .Map(dest => dest.UserId, src => src.UserId.Value);
+
+        config.NewConfig<Material, MaterialResponse>()
+            .Map(dest => dest.Id, src => src.Id.Value);
+
+        config.NewConfig<Labor, LaborResponse>()
+           .Map(dest => dest.Id, src => src.Id.Value);
     }
 }
